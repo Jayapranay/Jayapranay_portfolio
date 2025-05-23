2bb1e22487e64ff677630b818a479ad6534ba940
Jaya Pranay Guduru <160163507+Jayapranay@users.noreply.github.com>
2025-05-23 17:00:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+AOS.init();
+
+new Typed("#typed-text", {
+  strings: ["Jaya Pranay Guduru", "Frontend Developer", "Cloud Intern", "B.Tech Final Year"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
+
+function showProject(title) {
+  alert("More details about: " + title);
+}
