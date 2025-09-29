828d5705148ac86a842085b683a2c0f459e5cf4c
Jaya Pranay Guduru <160163507+Jayapranay@users.noreply.github.com>
2025-09-29 22:05:46 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-AOS.init();
-
-new Typed("#typed-text", {
-  strings: ["Jaya Pranay Guduru", "Frontend Developer", "Cloud Intern", "B.Tech Final Year"],
-  typeSpeed: 60,
-  backSpeed: 40,
-  loop: true
-});
